91e4d200bebf12649cf572a202edb168ec438cf3
Priyanshu Barsagade <priyanshu.barsagade@solera.com>
2026-03-19 19:02:26 +0530

    Removed backend folder from frontend branch

--- backend/ParkingAllocationBackend/DTO/RegisterDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ParkingAllocationBackend.DTO
-{
-    public class RegisterDto
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-}
--- backend/ParkingAllocationBackend/DTO/VehicleDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ParkingAllocationBackend.DTO
-{
-    public class VehicleDto
-    {
-        public int Id { get; set; }
-        public int UserId { get; set; }
-    }
-}
